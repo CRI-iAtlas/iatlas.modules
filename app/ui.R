4adbc268c3ae22f2ec28f00a9fd28c36e8546a47
Andrew Lamb <andrewelamb@gmail.com>
2021-12-17 08:43:29 -0800

    fix feature name and display issue, added example

--- app/ui.R
+++ app/ui.R
@@ -10,7 +10,8 @@ ui <- function() {
           barplot_ui("barplot1", title = "Example 1"),
           barplot_ui("barplot2", title = "Example 2"),
           barplot_ui("barplot3", title = "Example 3"),
-          barplot_ui("barplot4", title = "Example 4")
+          barplot_ui("barplot4", title = "Example 4"),
+          barplot_ui("barplot5", title = "Example 5")
         ),
         shiny::tabPanel(
           "Distribution Plots",
